e36ab61d7dd72db85f3c19de05871154516a7a63
Maryam <98962702+MaryamAli-2020@users.noreply.github.com>
2025-06-25 05:39:39 +0400

    darkmode

--- src/components/ui/CertificateCard.tsx
+++ src/components/ui/CertificateCard.tsx
@@ -77,7 +77,7 @@ const CertificateCard = ({ certificate, isVisible, delay }: CertificateCardProps
           href={certificate.certificateUrl}
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex items-center justify-center w-full text-teal-800 px-4 py-2 rounded-lg font-medium transition-all duration-300 "
+          className="inline-flex items-center justify-center w-full text-teal-800 dark:text-white px-4 py-2 rounded-lg font-medium transition-all duration-300 "
         >
           <span>View Certificate</span>
           <ExternalLink className="w-4 h-4 ml-2 group-hover:translate-x-1 transition-transform duration-300" />
